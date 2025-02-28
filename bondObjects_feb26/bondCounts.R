78efbfd6086687ecf1b0b438f818d39fce6d03ab
ecoronae <emmanuelcorona@169-231-60-235.wireless.ucsb.edu>
2025-02-27 20:17:34 -0800

    ihkjh

--- bondObjects_feb26/bondCounts.R
+++ bondObjects_feb26/bondCounts.R
@@ -1,22 +1,31 @@
 # Define the bondCounts class
 bondCounts <- setRefClass("bondCounts",
   fields = list(
-    bondDF = "list"
+    bondDF = "list",
+    valDate = 'Date',
+    pv = 'numeric'
   ),
   
   methods = list(
-    initialize = function() {
+    initialize = function(startDate) {
       .self$bondDF <<- list(Date = c(),
-                            bondType = c(),
+                            bondItem = c(),
                             unitsTraded = c(),
                             numberStripped = c())
+      .self$valDate <<- startDate
+      .self$pv <<- 0
     },
     
+    moveForward = function(doBondAction) {
+      
+    }
+      
+      
     bondAction = function(doBondAction) {
       bondActionKey <- c("buy" = 1, "sell" = -1, "strip" = 0)
       
       .self$bondDF$Date <- c(.self$bondDF$Date, doBondAction$getBondDate())
-      .self$bondDF$bondType <- c(.self$bondDF$bondType, doBondAction$getBondObj())
+      .self$bondDF$bondItem <- c(.self$bondDF$bondItem, doBondAction$getBondObj())
       .self$bondDF$unitsTraded <- c(.self$bondDF$unitsTraded, 
                                           doBondAction$getUnits() * bondActionKey[doBondAction$getName()])
       .self$bondDF$numberStripped <- c(.self$bondDF$numberStripped,
@@ -28,7 +37,7 @@ bondCounts <- setRefClass("bondCounts",
     },
     
     setBondDF = function(newBondDF) {
-      bondDF <- newBondDF
+      .self$bondDF <- newBondDF
     }
   )
 )
@@ -39,13 +48,15 @@ bondCounts <- setRefClass("bondCounts",
 bond1 <- bondType(0.03, '2023-03-06', '2024-03-06', 1, '30/365', TRUE)
 bond2 <- bondType(0.04, '2023-01-01', '2033-01-01', 10, '30/360', FALSE)
 
+bi1 <- bondItem(bond1, TRUE)
+bi2 <- bondItem(bond2, TRUE)
+
+act1 <- bondBuy(5, bi1, '2025-01-01')
+act2 <- bondSell(3, bi2, '2025-01-01')
+act3 <- bondSell(2, bi1, '2025-01-03')
+act4 <- bondStrip(1, bi2, '2026-01-03')
 
 bc <- bondCounts()
-
-act1 <- bondBuy(5, bond1, '2025-01-01')
-act2 <- bondSell(3, bond2, '2025-01-01')
-act3 <- bondSell(2, bond1, '2025-01-03')
-act4 <- bondStrip(1, bond2, '2026-01-03')
 
 bc$bondAction(act1)
 bc$bondAction(act2)
