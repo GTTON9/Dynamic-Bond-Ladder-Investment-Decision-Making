610a76f6a45329349db5f72d897872f096f45c09
SpecialTree <andersonsbestman@gmail.com>
2025-03-13 19:59:53 -0700

    We got forks to work!

--- aveBondSrc/bondPortfolio(IP).R
+++ aveBondSrc/bondPortfolio(IP).R
@@ -6,7 +6,6 @@ bondPortfolio <- setRefClass("bondPortfolio",
     bondLedger = 'ANY',
     cashPosition = "ANY",
     currDate = 'character',
-
     yieldObj = "ANY"
   ),
   
@@ -14,7 +13,6 @@ bondPortfolio <- setRefClass("bondPortfolio",
     initialize = function(bondLedger = NULL,cashPosition = NULL,currDate = NA_character_, 
                           yieldObj = NULL) {
       
-      .self$posList <- list()
       .self$bondLedger <- bondLedger
       .self$cashPosition <- cashPosition
       
